7457dcd0d318a90670a9ecd0a94a2f9840ec543f
Daria <dorothyfedorova@gmail.com>
2023-01-12 23:39:47 +0300

    corrected solution of task 21

--- task21/Program.cs
+++ task21/Program.cs
@@ -5,8 +5,9 @@
 А(3,6,8); В(2,1,-7) -> 15.84
 А(7,-5,0); В(1,-1,9) -> 11.53
 */
-
+/*
 Console.Clear();
+
 Console.WriteLine("Введите 3 цифры координаты точки A"); // запрашиваем у пользователя координаты как массив
 string a = Console.ReadLine();
 Console.WriteLine("Введите  3 цифры координаты точки B");
@@ -17,3 +18,27 @@ int[] b1 = { Convert.ToInt32(b[0].ToString()), Convert.ToInt32(b[1].ToString()),
 
 double distance = Math.Sqrt(Math.Pow((a1[0] - b1[0]), 2) + Math.Pow((a1[1] - b1[1]), 2) + Math.Pow((a1[2] - b1[2]), 2)); //  расчет расстояния между точками А и В
 Console.WriteLine("Расстояние между точками A и B = " + distance);
+*/
+
+Console.Clear();
+
+Console.Write("Для точки А введите x координаты:"); // задаем параметры точек
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Для точки А введите y координаты:");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Для точки А введите z координаты:");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Для точки B введите x координаты:");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Для точки B введите y координаты:");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Для точки B введите z координаты:");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+
+
+double distance = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));// Вычисляем расстояние между точками A и B
+Console.WriteLine("Расстояние между точками A и B = " + distance);
+

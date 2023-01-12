8cda3d2d4c3189343f2a6bfe68c05fc72506e8c1
Daria <dorothyfedorova@gmail.com>
2023-01-12 23:10:13 +0300

    add solution og task 23

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+/* ЗАДАЧА: напишите программу, которая принимает на вход число (n) и выдает таблицу кубов чисел от 1 до n
+3 -> 1,8,27
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число для возведение в куб: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(i * i * i);
+}
